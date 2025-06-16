1b8534438672c23bc2a43c721cd6c2dd8e079aeb
osmanforhad <osmanforhad93@gmail.com>
2025-06-16 09:12:24 +0600

    comment out previous test case

--- sum.test.js
+++ sum.test.js
@@ -1,3 +1,14 @@
+const sum = require('./sum');
+
+/**
+ * test function cotain a description 
+ * and
+ * a arrow function
+ */
+// test('to pluse to is four', () => {
+//     expect(sum(1, 2)).toBe(3); //to be used for premitive value like number boolian true false
+// });
+
 /**
  * test function cotain a description 
  * and
