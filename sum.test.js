f94a27c787e50bdc7b433a589b064f2143506bf9
osmanforhad <osmanforhad93@gmail.com>
2025-06-16 09:07:55 +0600

    writing some comment in the test file

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
 const sum = require('./sum');
 
+/**
+ * test function cotain a description 
+ * and
+ * a arrow function
+ */
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3); //to be used for premitive value like number boolian true false
 });
