b2ff67070fc5c3e23c1a58a15fc8585815856026
osmanforhad <osmanforhad93@gmail.com>
2025-06-16 09:26:42 +0600

    writing test case for false value and organize the commnet

--- sum.test.js
+++ sum.test.js
@@ -1,30 +1,33 @@
-const sum = require('./sum');
-
 /**
- * test function cotain a description 
+ * test function cotain a description
  * and
  * a arrow function
  */
+
+//writing test case for numaric addition
 // test('to pluse to is four', () => {
 //     expect(sum(1, 2)).toBe(3); //to be used for premitive value like number boolian true false
 // });
 
 /**
- * test function cotain a description 
+ * test function cotain a description
  * and
  * a arrow function
  */
+//writing test case for numaric addition
 // test('to pluse to is four', () => {
 //     expect(2+2).toBe(4); //tobe used for premitive value like number boolian true false
 // });
 
-/**
- * test function cotain a description 
- * and
- * a arrow function
- */
-test('object assignment', () => {
-    const data = {one:1};
-    data['two'] = 2;
-    expect(data).toEqual({one: 1, two: 2});
+//writing test case for equal value
+// test("object assignment", () => {
+//   const data = { one: 1 };
+//   data["two"] = 2;
+//   expect(data).toEqual({ one: 1, two: 2 });
+// });
+
+//writing test case for false value
+test("null is falsy", () => {
+  const n = null;
+  expect(n).toBeFalsy();
 });
